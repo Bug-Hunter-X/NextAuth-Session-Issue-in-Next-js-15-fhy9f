0be9819eef2df0947ee407578ebd7180ccd3f967
BugHunter-X <askzenai@gmail.com>
2025-01-02 16:34:07 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,30 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href='/about'>
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+import { unstable_getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]';
+
+export default async function About() {
+  const session = await unstable_getServerSession(authOptions);
+
+  if (!session) {
+    return <p>Access denied. Please login.</p>;
+  }
+
+  return <p>This is the about page. You are logged in!</p>;
+}
+```
